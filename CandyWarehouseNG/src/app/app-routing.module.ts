b34bdde090cbedb9e7b44e4ce778dec9d4544ac3
SeanGRev <sean.guo@revature.net>
2021-08-03 20:18:06 -0400

    Components and basic navbar created

--- CandyWarehouseNG/src/app/app-routing.module.ts
+++ CandyWarehouseNG/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EmployeeComponent } from './components/employee/employee.component';
+import { ManagerComponent } from './components/manager/manager.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { UserComponent } from './components/user/user.component';
+import { InventoryComponent } from './components/inventory/inventory.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {path: 'employee', component: EmployeeComponent},
+  {path: 'manager', component: ManagerComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'user', component: UserComponent},
+  {path: 'inventory', component: InventoryComponent},
+  {path: 'login', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
