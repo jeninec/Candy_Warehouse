4781cabf82785ab9a6501b90546ba4ab94e074db
SeanGRev <sean.guo@revature.net>
2021-08-04 15:29:11 -0400

    Frontend models

---
+++ CandyWarehouseNG/src/app/models/People.ts
@@ -0,0 +1,37 @@
+import * as internal from "stream";
+
+export class People{
+
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    zip: string;
+    phone: string;
+    title: string;
+
+
+    constructor(id: number, firstName: string, lastName: string, email: string, password: string,
+        address1: string, address2: string, city: string, state: string, zip: string,
+        phone: string, title: string){
+
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.title = title;
+
+    }
+}
