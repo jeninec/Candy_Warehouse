27b01fd2c0ce720f73762abcdd8bb84593f1a8bc
SeanGRev <sean.guo@revature.net>
2021-08-05 16:59:39 -0400

    Updated manager page

--- CandyWarehouseNG/src/app/components/manager/manager.component.ts
+++ CandyWarehouseNG/src/app/components/manager/manager.component.ts
@@ -16,6 +16,21 @@ export class ManagerComponent implements OnInit {
   isItems: boolean = true;
   selectOption: string = "Show Employees";
 
+  fakeItemsList: Array<any> = [
+    { "id": 1, "name": "Hershey's", "catagory": "chocolates", "price": 2,
+      "qty": 100, "description": "Generic American chocolate"}
+  ];
+
+  fakeEmployeeList: Array<any> = [
+    {"id": 1, "name": "Jack Frank", "email": "jf@gmail.com", "address1": "30 JackFrank St", 
+    "address2": "", "city" : "Frankville", "state": "Alabama", "zip": "09823", 
+    "phone": "902-232-1231", "title": "Employee"},
+    {"id": 2, "name": "Guido Murt", "email": "gm@gmail.com", "address1": "22 Street St", 
+    "address2": "", "city" : "Chicago", "state": "Illinois", "zip": "123123", 
+    "phone": "232-542-1231", "title": "Employee"}
+  ];
+
+
   toggleShow() {
     this.isEmps = ! this.isEmps;
     this.isItems = ! this.isItems;
