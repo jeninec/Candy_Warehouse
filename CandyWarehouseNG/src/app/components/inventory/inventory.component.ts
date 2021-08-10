7102afda57e8a90c1e9668f660af5703b32ad902
Tony Bahner <tony.bahner@revature.net>
2021-08-10 16:02:12 -0600

    added to functions to inventory page

--- CandyWarehouseNG/src/app/components/inventory/inventory.component.ts
+++ CandyWarehouseNG/src/app/components/inventory/inventory.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpAllCandyService } from 'src/app/services/http-all-candy.service';
 import { Item } from 'src/app/models/Item';
+import { HttpOrderService } from 'src/app/services/http-order.service';
+import { Order } from 'src/app/models/Order';
+import { Observable } from 'rxjs';
+import { HttpPeopleService } from 'src/app/services/http-people.service';
 
 @Component({
   selector: 'app-inventory',
@@ -9,7 +13,7 @@ import { Item } from 'src/app/models/Item';
 })
 export class InventoryComponent implements OnInit {
 
-  constructor(private allCandy : HttpAllCandyService) { }
+  constructor(private allCandy : HttpAllCandyService, private newOrder : HttpOrderService, private putPerson : HttpPeopleService) { }
 
   ngOnInit(): void {
     this.getAllCandy();
@@ -49,5 +53,33 @@ export class InventoryComponent implements OnInit {
       }
     )
   }
+
+  
+  buildOrder: Order = new Order(0, 0, "", "", false, []);
+  // Starts an empty order
+  startNewOrder() {
+    this.newOrder.addOrder(this.buildOrder).subscribe(
+      (response) => {
+        console.log(response);
+      }
+    ) 
+  }
+
+  itemsInCart: Item[] = [];
+  // Updates the order with the list of itemsInCart[]
+  updateCart() {
+    this.newOrder.updateOrder(this.buildOrder).subscribe(
+      (response) => {
+        response.itemId = this.itemsInCart;
+      }
+    )
+  }
+
+  addOrderToPerson() {
+
+    // this.putPerson.getPersonByEmail()
+
+  }
+  
 
 }
