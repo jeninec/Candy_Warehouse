5dcba4ea708b6ee57843f32cb25682fc2bbaa1e0
Jenine <jenine.c@revature.net>
2021-08-10 14:46:22 -0400

    adding user page to display and edit user info

--- CandyWarehouseNG/src/app/components/user/user.component.ts
+++ CandyWarehouseNG/src/app/components/user/user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpPeopleService } from 'src/app/services/http-people.service';
+import { PeopleService } from 'src/app/services/people.service'
+import { People } from 'src/app/models/People';
+import { ActivatedRoute, Router } from '@angular/router'
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router, private peopleHttp: HttpPeopleService, private peopleServ: PeopleService) { }
 
   ngOnInit(): void {
+    if(localStorage.getItem("title") == "admin" || localStorage.getItem("title") == "manager" || localStorage.getItem("title") == "employee" || localStorage.getItem("title") == "buyer"){
+
+      this.displayInfo();
+    }
+    else {
+      this.router.navigateByUrl("/home");
+    }
   }
+
+  showInfo: boolean = true;
+  editInfo: boolean = false;
+
+  //uid: string = localStorage.getItem("id").toString();
+  user: People = new People(0, "", "", "", "", "", "", "" , "", "", "", "", []);
+
+  displayInfo() {
+    this.showInfo = true;
+    this.editInfo = false;
+
+  
+
+    var uid = this.route.snapshot.paramMap.get('id');
+    var til = this.route.snapshot.paramMap.get('title');
+    var email = this.route.snapshot.paramMap.get('email');
+    console.log(uid);
+    console.log(til);
+    console.log(email);
+    
+      if(uid == null){
+        uid = "4";
+      }
+
+    this.peopleHttp.getPeople(uid).subscribe(
+    
+      (response) => {
+        console.log(response);
+        this.peopleServ.people = response;
+        this.user = response;
+      }
+    )
+
+  }
+
+  changeInfo() {
+    this.showInfo = false;
+    this.editInfo = true;
+    
+  }
+    
+  updateEmp() {
+      console.log(this.peopleServ.people);
+
+    this.peopleHttp.updatePeople(this.peopleServ.people).subscribe(
+      (response) => {
+        alert("Movie Successfully Updated");
+        this.router.navigateByUrl("/movies");
+      }
+    );
+  }
+
 
 }
