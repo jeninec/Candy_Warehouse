1b40e5e50e8decb0b6dcbf90d467c7f6fcada964
stukruze <audiolaboratory@hotmail.com>
2021-08-12 17:35:00 -0400

    minor tweeks

--- CandyWarehouseNG/src/app/components/login/login.component.ts
+++ CandyWarehouseNG/src/app/components/login/login.component.ts
@@ -30,7 +30,11 @@ export class LoginComponent implements OnInit {
           this.responseMessage = "Success! Logging in...";
           localStorage.setItem("title", user.title);
           localStorage.setItem("email", user.email);
-          location.reload();
+
+          window.setTimeout(()=>{
+            location.reload();
+         }, 1500);
+          
          }else {  
            console.log("Incorrect credentials");
            this.responseMessage = "Incorrect credentials";
@@ -46,7 +50,9 @@ export class LoginComponent implements OnInit {
     localStorage.removeItem("title");
     localStorage.removeItem("email");
     this.responseMessage = "Logging out";
-    location.reload();
+    window.setTimeout(()=>{
+      location.reload();
+   }, 1500);
   }
 
 }
